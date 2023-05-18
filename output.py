d5e5875f09269d5cb0510e2088afb3dfbc41698b
Om Singh <113787072+indeterminatediv@users.noreply.github.com>
2023-05-18 15:19:08 +0530

    Update output.py

--- output.py
+++ output.py
@@ -1,28 +1,36 @@
+import streamlit as st
 import cv2
 import numpy as np
 from keras.models import load_model
 
+st.title(':blue[FACE EXPRESSION RECOGNIZER]:sunglasses:')
+st.text('Upload image or images on a single slide to identify face expressions')
+
 model = load_model('model_file.h5')
-
 faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+labels_dict = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Neutral', 5: 'Sad', 6: 'Surprise'}
 
-labels_dict={0:'Angry',1:'Disgust',2:'Fear',3:'Happy',4:'Neutral',5:'Sad',6:'Surprise'}
-frame=cv2.imread("image.png")
-gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-faces=faceDetect.detectMultiScale(gray, 1.3, 3)
-for x,y,w,h in faces:
-    sub_face_img = gray[y:y+h, x:x+w]
-    resized=cv2.resize(sub_face_img,(48,48))
-    normalize=resized/255.0
-    reshaped=np.reshape(normalize, (1,48,48,1))
-    result=model.predict(reshaped)
-    label=np.argmax(result, axis=1)[0]
-    print(label)
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 1)
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255), 2)
-    cv2.rectangle(frame, (x,y-40), (x+w, y), (50,50,255), -1)
-    cv2.putText(frame, labels_dict[label], (x,y-10), cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    st.image(frame, caption="Uploaded Image", use_column_width=True)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceDetect.detectMultiScale(gray, 1.3, 3)
     
-cv2.imshow("Frame",frame)
-cv2.waitKey(0)
-cv2.destroyAllWindows()    
+    for x, y, w, h in faces:
+        sub_face_img = gray[y:y+h, x:x+w]
+        resized = cv2.resize(sub_face_img, (48, 48))
+        normalize = resized / 255.0
+        reshaped = np.reshape(normalize, (1, 48, 48, 1))
+        result = model.predict(reshaped)
+        label = np.argmax(result, axis=1)[0]
+
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 1)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (50, 50, 255), 2)
+        cv2.rectangle(frame, (x, y-40), (x+w, y), (50, 50, 255), -1)
+        cv2.putText(frame, labels_dict[label], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+
+    st.image(frame, caption="Image with expressions marked", use_column_width=True)
